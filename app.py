6ccc7158a3b87e10c694b2718f237b356d1129f6
Wei <vaithwee@yeah.net>
2023-04-13 17:16:47 +0800

    Added qiniu

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask
+from flask import Flask, request
+import qiniu
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/token')
+def gen_token():
+    ak = request.args['ak']
+    sk = request.args['sk']
+    key = request.args['key']
+    bucket = request.args['bucket']
+    q = qiniu.Auth(access_key=ak, secret_key=sk)
+    if key:
+        bucket = bucket+":"+key
+    token = q.upload_token(bucket)
+    return token
+
+@app.route('/url')
+def gen_private_url():
+    ak = request.args['ak']
+    sk = request.args['sk']
+    url = request.args['url']
+    q = qiniu.Auth(access_key=ak, secret_key=sk)
+    pu = q.private_download_url(url)
+    return pu
